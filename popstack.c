f1b3415f059e5e8153093889ebd4fb49f0568122
roquesrolando <roquesrolando@hotmail.com>
2021-05-12 20:48:32 +0000

    update

---
+++ popstack.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+int line_count;
+/**
+ * pop - removes the top element
+ * @stack: the stack
+ * @param: the parameter
+ */
+
+void op_pop(stack_t **stack, __attribute__((unused))unsigned int param)
+{
+	stack_t *temp;
+
+	if (*stack == NULL)
+	{
+		dprintf(2, "L%d: can't pop an empty stack\n", line_count);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	(*stack) = (*stack)->next;
+	if (*stack != NULL)
+	{
+		(*stack)->prev = NULL;
+		free(temp);
+	}
+}
