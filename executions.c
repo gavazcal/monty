1023a48a20a5ff04d89ce187fa984323e3372996
roquesrolando <roquesrolando@hotmail.com>
2021-05-12 18:50:38 +0000

    more error message

--- executions.c
+++ executions.c
@@ -1,5 +1,5 @@
 #include "monty.h"
-
+int line_count;
 /**
  * executions - takes the input and compares it to our opcodes
  * @opcode: opcode being pass on
@@ -21,6 +21,12 @@ void executions(char *opcode, stack_t **stack, unsigned int param)
 		if (strcmp(opcode, opcodes[i].opcode) == 0)
 		{
 			(opcodes[i].f)(stack, param);
+			return;
 		}
+	}
+	if (opcodes[i].opcode == NULL)
+	{
+		dprintf(2, "L%d: unknown instruction %s\n", line_count, opcode);
+		exit(EXIT_FAILURE);
 	}
 }
